e3a5022b996b85eb2f514f762bc2d829d5eb65ff
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2016-12-02 11:30:11 +1100

    feat(deps): Remove dependencies

--- project/Depend.scala
+++ project/Depend.scala
@@ -3,22 +3,12 @@ import sbt._
 object Depend {
   lazy val scalazVersion = "7.1.7"
 
-  lazy val http4sVersion = "0.14.11"
-
-  lazy val scalaz = Seq(
-    "org.scalaz" %% "scalaz-core"
-  ).map(_ % scalazVersion)
-
   lazy val fs2 = Seq(
     "co.fs2" %% "fs2-core" % "0.9.2"
   )
 
   lazy val awsSqs = Seq(
     "com.amazonaws" % "aws-java-sdk-sqs" % "1.11.33"
-  )
-
-  lazy val argonaut = Seq(
-    "io.argonaut" %% "argonaut" % "6.1"
   )
 
   lazy val scalaTestCheck = Seq(
@@ -32,9 +22,7 @@ object Depend {
   )
 
   lazy val dependencies = 
-    scalaz ++
     fs2 ++
     awsSqs ++
-    argonaut ++
     scalaTestCheck
 }
